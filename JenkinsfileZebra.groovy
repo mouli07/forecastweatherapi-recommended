84dd1de5d31009e8b137147b0d8e58f73ff93d37
Thacker <riddhi.thacker@capgemini.com>
2018-11-19 12:56:03 +0530

    first commit 1

---
+++ JenkinsfileZebra.groovy
@@ -0,0 +1,42 @@
+pipeline {
+    agent any
+    tools {
+        jdk 'jdk8'
+        maven 'maven3'
+    }
+    
+    stages {
+        stage('download_stash_code') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('clean') {
+            steps {
+                sh "mvn clean"
+            }
+        }
+
+       1
+
+        stage('tag the build') {
+            steps { 
+
+                    withCredentials([
+                        [$class: 'UsernamePasswordMultiBinding', credentialsId: '7943607d-b421-4237-bc45-c7cef3fb3904', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS'],
+                            ]){     
+
+                                    sh """(
+                                     mvn install -Ptest -Dusername=riddhi_thacker@yahoo.com -Dpassword=Ridz94_@
+                                    )"""
+                                }
+                                
+
+            }
+        }        
+        
+    }
+    
+}
+
